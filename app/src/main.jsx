114a12b1248f067cf79597f4f76d37d4eb5d2285
Ryan E Davis <redavi19@asu.edu>
2026-09-01 12:35:00 -0400

    Use streamlined I Computer Anything application shell

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./AppClean.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
